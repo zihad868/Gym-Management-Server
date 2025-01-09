2a997d393f0213c92ddeeeea1ce257b2282b3d7a
zihad868 <zihadh868@gmail.com>
2025-01-09 11:22:10 +0600

    Update Code

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { registerAdmin, loginAdmin, createClassSchedule } from '../controller/adminController';
+import { authenticate } from '../middleware';
 
 const router = Router();
 
 router.post('/admin-register', registerAdmin);
 router.post('/admin-login', loginAdmin);
-router.post('/assign-trainer', createClassSchedule);
+router.post('/assign-trainer', authenticate, createClassSchedule);
 
 export default router;
